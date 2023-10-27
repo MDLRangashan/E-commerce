a01960e67093ce3c8e5820ec99f9cd2fbc5f3f52
MDLRangashan <lasindurangashan225@gmail.com>
2023-10-28 03:19:17 +0530

    exclusive offer area

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -8,7 +8,7 @@ const item = (props) => {
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-price-new">
-                {props.new_price}
+                ${props.new_price}
             </div>
             <div className="item-price-old">
                 {props.old_price}
